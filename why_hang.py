bfef4a5bbad931de22941661b5b59092348f7a8f
Geoff Greer <geoff@greer.fm>
2012-02-01 11:21:55 -0800

    Classy

--- why_hang.py
+++ why_hang.py
@@ -4,38 +4,40 @@ import traceback
 
 from twisted.internet import defer, task
 
-# Usage: just import why_hang and stuff magically gets printed out if the main thread hangs. The hang time to trigger a traceback is somewhat probablistic. Worst case, the reactor hangs for MAX_DELAY seconds.
+# Usage: just import why_hang and stuff magically gets printed out if the main thread hangs for more than MAX_DELAY seconds.
 
 # These values are seconds
 CANCEL_INTERVAL = 0.1
 MAX_DELAY = 0.5
 
 
-def reset_itimer():
-    # TODO: change this to ITIMER_VIRTUAL for real-life usage
-    signal.setitimer(signal.ITIMER_REAL, MAX_DELAY)
+class WhyHang(object):
+    def __init__(self):
+        """docstring for __init__"""
+        # Handle SIGALRMs with print_traceback
+        signal.signal(signal.SIGALRM, self.log_traceback)
 
 
-def log_traceback(signal, frame):
-    # Oh snap, cancel_sigalrm didn't get called
-    traceback.print_stack(frame)
-    reset_itimer()
+        # this LoopingCall is run by the reactor.
+        # If the reactor is hung, cancel_sigalrm won't run and the handler for SIGALRM will fire
+        self.lc = task.LoopingCall(self.cancel_sigalrm)
+        self.lc.start(CANCEL_INTERVAL)
+
+    def reset_itimer(self):
+        # TODO: change this to ITIMER_VIRTUAL for real-life usage
+        signal.setitimer(signal.ITIMER_REAL, MAX_DELAY)
 
 
-def cancel_sigalrm():
-    # Cancel any pending alarm
-    if signal.alarm(0) != 0:
-        print "Previous alarm cancelled"
-    else:
-        print "No SIGALRM to cancel"
-    reset_itimer()
+    def log_traceback(self, signal, frame):
+        # Oh snap, cancel_sigalrm didn't get called
+        traceback.print_stack(frame)
+        self.reset_itimer()
 
 
-# Handle SIGALRMs with print_traceback
-signal.signal(signal.SIGALRM, log_traceback)
+    def cancel_sigalrm(self):
+        # Cancel any pending alarm
+        if signal.alarm(0) == 0:
+            print "No SIGALRM to cancel. This should only happen if we handled a traceback"
+        self.reset_itimer()
 
 
-# this LoopingCall is run by the reactor.
-# If the reactor is hung, cancel_sigalrm won't run and the handler for SIGALRM will fire
-lc = task.LoopingCall(cancel_sigalrm)
-lc.start(CANCEL_INTERVAL)
