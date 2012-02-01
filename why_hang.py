9fa6591edf9ec4bd26f531135192da06949d74ce
Geoff Greer <geoff@greer.fm>
2012-02-01 11:04:40 -0800

    Cleanup.

--- why_hang.py
+++ why_hang.py
@@ -4,27 +4,33 @@ import traceback
 
 from twisted.internet import defer, task
 
-# Usage: just import why_hang and stuff magically gets printed out if the main thread hangs for > .01 seconds
+# Usage: just import why_hang and stuff magically gets printed out if the main thread hangs. The hang time to trigger a traceback is somewhat probablistic. Worst case, the reactor hangs for SIGALRM_INTERVAL + MAX_DELAY seconds.
 
-SIGALRM_INTERVAL = 0.1 # seconds
+# These values are seconds
+SIGALRM_INTERVAL = 0.1
+MAX_DELAY = 0.2
 
 def print_traceback(signal, frame):
-    # Oh snap, we didn't
+    # Oh snap, cancel_sigalrm didn't get called
     traceback.print_stack(frame)
-    #import pdb; pdb.set_trace()
+    set_itimer()
 
-def cancel_sigalrm_and_set_itimer():
+def cancel_sigalrm():
     # Cancel any pending alarm
     if signal.alarm(0) != 0:
         print "Previous alarm cancelled"
     else:
         print "No SIGALRM to cancel"
-    # Send SIGALRM every 0.11 seconds
+    set_itimer()
+
+def set_itimer():
     # TODO: change this to ITIMER_VIRTUAL for real-life usage
-    signal.setitimer(signal.ITIMER_REAL, SIGALRM_INTERVAL + 0.01)
+    signal.setitimer(signal.ITIMER_REAL, SIGALRM_INTERVAL + MAX_DELAY)
 
 # Handle SIGALRMs with print_traceback
 signal.signal(signal.SIGALRM, print_traceback)
 
-lc = task.LoopingCall(cancel_sigalrm_and_set_itimer)
+# this LoopingCall is run by the reactor. 
+# If the reactor is hung, cancel_sigalrm won't run and the handler for SIGALRM will fire
+lc = task.LoopingCall(cancel_sigalrm)
 lc.start(SIGALRM_INTERVAL)
